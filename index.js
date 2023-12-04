211ae9c905a3872eeead2e227a3720e87419f50d
Replit user <>
2023-12-04 19:25:33 +0000

    feat: require body-parser for Post request and create mongoose schema and model for urls

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Create a body-parser object function
+let bodyParser = require('body-parser');
+// Use body-parser to Parse POST Requests
+app.use(bodyParser.urlencoded({extended: false}));
+
 // Add mongoose
 const mongoose = require('mongoose');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Define Schema
+const urlSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+      type: String,
+      required: true
+    }
+});
 
+let url;
+
+// Create model and assign it to url variable
+url = mongoose.model('url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
